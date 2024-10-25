df3e5f1f817c87a670ee577a15a56abd9914d2a1
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-25 18:19:44 +0600

    1. Created Home component to display all posts from the db 2. fetchData - makes a get request to the db and sorts posts by date of their creation, then - displays this data from useEffect on initial page load 3. Displayed all posts from db using map method

---
+++ src/Containers/Home/Home.tsx
@@ -0,0 +1,60 @@
+import { useCallback, useEffect, useState } from 'react';
+import { IPost, IPostAPI } from '../../types';
+import axiosAPI from '../../axiosAPI.ts';
+import Post from '../../Components/Post/Post.tsx';
+import Loader from '../../Components/UI/Loader/Loader.tsx';
+
+const Home = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [loader, setLoader] = useState<boolean>(false);
+
+  const fetchData = useCallback(async () => {
+    try {
+      setLoader(true);
+      const response: { data: IPostAPI } = await axiosAPI<IPostAPI>('/posts.json');
+
+      if (response.data) {
+        const gamesAPI: IPost[] = Object.keys(response.data).map(postKey => {
+          return {
+            ...response.data[postKey],
+            id: postKey,
+          };
+        });
+
+        const sortedPosts = gamesAPI.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
+        setPosts(sortedPosts);
+      }
+    } catch (e) {
+      console.error(e);
+      setLoader(false);
+    } finally {
+      setLoader(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    void fetchData();
+  }, [fetchData]);
+
+  return (
+    <>
+      {loader ? <Loader/> :
+        <>
+          <div className="container d-flex flex-column align-items-center gap-3">
+            {posts.length === 0 ? <p className="fs-4">No posts found</p> :
+              posts.map(post => (
+                <Post
+                  key={post.id}
+                  post={post}
+                />
+              ))
+            }
+          </div>
+        </>
+      }
+    </>
+  );
+};
+
+export default Home;
