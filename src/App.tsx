30014a69a8b24f4b1dfb9cb11800e7c70a226f88
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-25 18:21:13 +0600

    Added routing across all application pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Containers/Home/Home.tsx';
+import EditPost from './Containers/EditPost/EditPost.tsx';
+import Navigation from './Components/Navigation/Navigation.tsx';
+import About from './Containers/About/About.tsx';
+import AddPost from './Containers/AddPost/AddPost.tsx';
+import Contacts from './Containers/Contacts/Contacts.tsx';
+import PostInfo from './Components/PostInfo/PostInfo.tsx';
+
 const App = () => {
-  return <></>;
+  return <>
+    <header>
+      <Navigation/>
+    </header>
+
+    <div className="container mt-5">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="posts" element={<Home/>}/>
+        <Route path="/posts/new-post" element={<AddPost/>}/>
+        <Route path="/posts/:postId" element={<PostInfo/>}/>
+        <Route path="posts/:postId/edit" element={<EditPost/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contacts" element={<Contacts/>}/>
+        <Route path="*" element={<h2>Page not found</h2>}/>
+      </Routes>
+    </div>
+  </>;
 };
 
 export default App;
